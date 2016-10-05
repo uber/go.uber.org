e9f037f07463a9c45c16ed65e21c9a487193ac9f
Abhinav Gupta <abg@uber.com>
2016-10-04 17:14:47 -0700

    Workaround for subpackages issue
    Until #15 is fully deployed, run `scripts/update-everything.sh` from the root
    of this repo to make stuff work.

---
+++ scripts/update-modules.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Workaround until https://github.com/uber-go/sally/pull/15 is deployed
+
+name="$1"
+repo="$2"
+
+if [[ -z "$repo" ]]; then
+	echo "USAGE: $0 NAME REPO"
+	exit 1
+fi
+
+set -e
+
+workdir=$(mktemp -d)
+trap 'rm -rf "$workdir"' EXIT
+
+files=$(mktemp)
+git clone "https://$repo" "$workdir"
+( \
+	pushd "$workdir" >/dev/null; \
+	find . -name .git -prune -o -type d -print; \
+	popd >/dev/null \
+) | while read -r package; do
+		mkdir -p "docs/$name/$package"
+		cp "docs/$name.html" "docs/$name/$package/index.html"
+		echo "docs/$name/$package/index.html" >> "$files"
+	done
+
+git add $(cat "$files")
